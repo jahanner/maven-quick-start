cf2209bd53f6b40cf990ef1c8be33844a97c862d
jahanner7@gmail.com <jahanner7@gmail.com>
2018-03-20 10:46:19 -0500

    Fixed slf4j logging

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,18 @@
 package clinic.programming.training;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.commons.lang3.StringUtils;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class Application {
 
   public int countWords(String words) {
-    String[] seperateWords = StringUtils.split(words, ' ');
-    return (seperateWords == null) ? 0 : seperateWords.length;
+    String[] separateWords = StringUtils.split(words, ' ');
+    return (separateWords == null) ? 0 : separateWords.length;
   }
 
   public void greet() {
@@ -21,12 +25,19 @@ public class Application {
         System.out.println ("Inside Application");
     }
 
+    private static final Logger logger = getLogger(Application.class);
+
+
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	     Application app = new Application();
-       app.greet();
-       int count = app.countWords("I have four words");
-       System.out.println("Word count: " + count);
+    	Application app = new Application();
+        app.greet();
+        String test = "lasdj";
+        logger.info("Hello world");
+        logger.error("error");
+        logger.debug("Print: {} ", test);
+        int count = app.countWords("I have four words");
+        System.out.println("Word count: " + count);
     }
 }
